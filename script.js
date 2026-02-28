03aa6df5be680bdab14207e411a741235f5176b6
Riyaz Mohammad <riyazm55@yahoo.in>
2026-02-28 10:29:06 -0800

    small changes

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+const buttons = document.querySelectorAll('.btn-container .btn');
+
+buttons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    btn.classList.add('clicked');
+
+    // handle existing actions
+
+    setTimeout(() => btn.classList.remove('clicked'), 5000);
+  });
+});
